d49967243b8b392acf3a50d6da79f2ab0bd68027
e-zhang09 <36081769+e-zhang09@users.noreply.github.com>
2020-03-28 04:31:16 -0700

    added more images

--- js/main.js
+++ js/main.js
@@ -74,40 +74,7 @@ $(document).ready(function () {
             }
         });
 
-    $('#copyright').html('Copyright &copy;'+new Date().getFullYear()+' All rights reserved | Made with <i aria-hidden="true" class="fa fa-heart-o"></i> by <a href="./" rel="noopener" target="_blank">FUHSD Gives</a>\n');
-
-    // -------   Mail Send ajax
-
-    $(document).ready(function () {
-        var form = $('#myForm'); // contact form
-        var submit = $('.submit-btn'); // submit button
-        var alert = $('.alert-msg'); // alert div for show alert message
-
-        // form submit event
-        form.on('submit', function (e) {
-            e.preventDefault(); // prevent default form submit
-
-            $.ajax({
-                url: 'mail.php', // form action url
-                type: 'POST', // form submit method get/post
-                dataType: 'html', // request type html/json/xml
-                data: form.serialize(), // serialize form data
-                beforeSend: function () {
-                    alert.fadeOut();
-                    submit.html('Sending....'); // change submit button text
-                },
-                success: function (data) {
-                    alert.html(data).fadeIn(); // fade in response data
-                    form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");
-                    ; // reset submit button text
-                },
-                error: function (e) {
-                    console.log(e)
-                }
-            });
-        });
-    });
+    $('#copyright').html('Copyright &copy;' + new Date().getFullYear() + ' All rights reserved | Made with <i aria-hidden="true" class="fa fa-heart-o"></i> by <a href="./" rel="noopener" target="_blank">FUHSD Gives</a>\n');
 
 
 });

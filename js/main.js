835b370b3fc4faf5a574d0155835af578bbc30b0
e-zhang09 <36081769+e-zhang09@users.noreply.github.com>
2020-04-14 05:37:35 -0700

    added venmo support (diff website)

--- js/main.js
+++ js/main.js
@@ -127,6 +127,8 @@ $(document).ready(function () {
 
     if (isMobile) {
         let $pContainer = $('#projectsContainer');
+        $('.project-area .project-wrap .details').carouselHeights();
+
         $pContainer.data('ride', 'carousel').data('interval', 8000).addClass('slide').addClass('carousel');
         $('#projectsContainer > div > div > div > a > div').addClass('not-mobile');
         $('#projectsContainer > div > div > div >  div').addClass('not-mobile');
@@ -141,6 +143,7 @@ $(document).ready(function () {
             element: $pContainer.get()[0],
             handler: function () {
                 $pContainer.carousel();
+
                 this.destroy();
             },
             offset: function () {
@@ -523,7 +526,40 @@ function tableAjaxRequest(params) {
         '--\n' +
         'Together, we will weather this storm.\n' +
         'https://santaclaragives.org\n'
-    )
+    );
+
+    // Normalize Carousel Heights - pass in Bootstrap Carousel items.
+    $.fn.carouselHeights = function() {
+
+        var items = $(this), //grab all slides
+            heights = [], //create empty array to store height values
+            tallest; //create variable to make note of the tallest slide
+
+        var normalizeHeights = function() {
+
+            items.each(function() { //add heights to array
+                heights.push($(this).outerHeight(true));
+            });
+            tallest = Math.max.apply(null, heights); //cache largest value
+            items.each(function() {
+                $(this).css('height',(tallest + 10) + 'px');
+            });
+        };
+
+        normalizeHeights();
+
+        $(window).on('resize orientationchange', function () {
+            //reset vars
+            tallest = 0;
+            heights.length = 0;
+
+            items.each(function() {
+                $(this).css('min-height','0'); //reset min-height
+            });
+            normalizeHeights(); //run it again
+        });
+
+    };
 }
 
 function checkVisible(elm) {
